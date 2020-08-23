e0eae91b8284d6e513af3c6747b0d6fb94959652
Tofit0 <christophe.mathiou@gmail.com>
2020-08-23 17:07:29 +0200

    chp2 moteur template twig

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -2,9 +2,11 @@
 
 namespace OC\PlatformBundle\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request; // Nouveau use
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 //use Twig\Environment;
 
 class AdvertController extends AbstractController
@@ -12,18 +14,35 @@ class AdvertController extends AbstractController
     //public function indexAction(Environment $twig)
     public function indexAction($page)
     {
-        // On ne sait pas combien de pages il y a
-        // Mais on sait qu'une page doit être supérieure ou égale à 1
         if ($page < 1) {
-            // On déclenche une exception NotFoundHttpException, cela va afficher
-            // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
-            throw $this->createNotFoundException('Page "'.$page.'" inexistante.');
+            throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
         }
 
-        // Ici, on récupérera la liste des annonces, puis on la passera au template
+        // Notre liste d'annonce en dur
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
 
-        // Mais pour l'instant, on ne fait qu'appeler le template
-        return $this->render('@OCPlatform/Advert/index.html.twig');
+        return $this->render('@OCPlatform/Advert/index.html.twig', array(
+            'listAdverts' => $listAdverts,
+        ));
     }
 
     /**
@@ -101,5 +120,21 @@ class AdvertController extends AbstractController
         return $response;
     }
 
+    public function menuAction($limit)
+    {
+        // On fixe en dur une liste ici, bien entendu par la suite
+        // on la récupérera depuis la BDD !
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('@OCPlatform/Advert/menu.html.twig', array(
+            // Tout l'intérêt est ici : le contrôleur passe
+            // les variables nécessaires au template !
+            'listAdverts' => $listAdverts
+        ));
+    }
 
 }
