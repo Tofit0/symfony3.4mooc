3b8a22af2b9e6b92cbac5a73a51d33ca3191fafb
Tofit0 <christophe.mathiou@gmail.com>
2020-09-23 20:14:24 +0200

    chp4, part 4, les services utilisation poussées les tags

--- src/OC/PlatformBundle/Twig/AntispamExtension.php
+++ src/OC/PlatformBundle/Twig/AntispamExtension.php
@@ -17,6 +17,17 @@ class AntispamExtension extends \Twig_Extension
         $this->ocAntispam = $ocAntispam;
     }
 
+
+    // Twig va exécuter cette méthode pour savoir quelle(s) fonction(s) ajoute notre service
+    // Dans ce cas on pourra exécuter la fonction checkIfSpam depuis les vues twigs
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('checkIfSpam', array($this, 'checkIfArgumentIsSpam')),
+        );
+    }
+
+
     public function checkIfArgumentIsSpam($text)
     {
         return $this->ocAntispam->isSpam($text);
